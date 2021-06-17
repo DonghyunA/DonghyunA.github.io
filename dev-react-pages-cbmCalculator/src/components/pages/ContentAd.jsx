294f773292b93ff5d7450353bb4afe83ef7c7afa
DonghyunK <kdhkdk89@naver.com>
2021-06-17 20:56:05 +0900

    add cbmpage ad

--- dev-react-pages-cbmCalculator/src/components/pages/ContentAd.jsx
+++ dev-react-pages-cbmCalculator/src/components/pages/ContentAd.jsx
@@ -1,22 +1,30 @@
 import React from "react";
+import AdSense from "react-adsense";
 
 const ContentAd = () => {
   return (
-    <>
-      <script
-        async
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
-      ></script>
-      <ins
-        class="adsbygoogle"
-        style={{display:"block", textAlign:"center"}}
-        data-ad-layout="in-article"
-        data-ad-format="fluid"
-        data-ad-client="ca-pub-5708410027420247"
-        data-ad-slot="1754176350"
-      ></ins>
-      <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
-    </>
+    // <>
+    //   <script
+    //     async
+    //     src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+    //   ></script>
+    //   <ins
+    //     class="adsbygoogle"
+    //     style={{ display: "block", textAlign: "center" }}
+    //     data-ad-layout="in-article"
+    //     data-ad-format="fluid"
+    //     data-ad-client="ca-pub-5708410027420247"
+    //     data-ad-slot="1754176350"
+    //   ></ins>
+    //   <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
+    // </>
+    <AdSense.Google
+      style={{ display: "block", textAlign: "center" }}
+      layout="in-article"
+      format="fluid"
+      client="ca-pub-5708410027420247"
+      slot="1754176350"
+    ></AdSense.Google>
   );
 };
 
--- dev-react-pages-cbmCalculator/src/components/pages/SideAd.jsx
+++ dev-react-pages-cbmCalculator/src/components/pages/SideAd.jsx
@@ -1,22 +1,31 @@
 import React from "react";
+import AdSense from "react-adsense";
 
 const SideAd = () => {
   return (
-    <>
-      <script
-        async
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
-      ></script>
-      <ins
-        class="adsbygoogle"
-        style={{display:"block"}}
-        data-ad-client="ca-pub-5708410027420247"
-        data-ad-slot="4542608339"
-        data-ad-format="auto"
-        data-full-width-responsive="true"
-      ></ins>
-      <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
-    </>
+    // <>
+    //   <script
+    //     async
+    //     src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+    //   ></script>
+    //   <ins
+    //     class="adsbygoogle"
+    //     style={{ display: "block" }}
+    //     data-ad-client="ca-pub-5708410027420247"
+    //     data-ad-slot="4542608339"
+    //     data-ad-format="auto"
+    //     data-full-width-responsive="true"
+    //   ></ins>
+    //   <script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
+    // </>
+    <AdSense.Google
+    style={{ display: "block", textAlign: "center" }}
+        style={{ display: "block" }}
+        client="ca-pub-5708410027420247"
+        slot="4542608339"
+        format="auto"
+        responsive="true"
+  ></AdSense.Google>
   );
 };
 
